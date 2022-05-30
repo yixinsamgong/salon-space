2eb869b583c32ec9243e80843db819291c1edfac
Yixin Gong <sam19931225@gmail.com>
2022-05-30 16:52:13 -0400

    more app added

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div>App</div>
+    <div>{App}</div>
   )
 }
 
--- src/components/Poem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Poem() {
-  return (
-    <div>Poem</div>
-  )
-}
-
-export default Poem
