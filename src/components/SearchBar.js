326cd251a8d416d30e6b5c4b3fd44a01c305a1e8
Yixin Gong <sam19931225@gmail.com>
2022-05-31 16:36:24 -0400

    single file

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,32 +1,26 @@
 import React from 'react'
 
-<<<<<<< HEAD
-function SearchBar() {
-  return (
-    <div>SearchBar</div>
-  )
-}
-=======
-function SearchBar(searchTerm, onSearchChange) {
+function SearchBar({searchTerm,onSearchChange}) {
+  
 
-      // function handleSubmit(e) {
-      //   e.preventDefault();
-      //   onSearchChange(searchTerm)
-      // }
+      function handleSubmit(e) {
+        e.preventDefault();
+        onSearchChange(searchTerm)
+      }
+    
       
         return (
-          <div className="searchbar">
+          <div className="searchbar" onSubmit={handleSubmit}>
             <label htmlFor="search">Search Artworks:</label>
             <input
               type="text"
               id="search"
-              placeholder="Type a name to search..."
-              // value={searchTerm}
-              // onChange={(e) => onSearchChange(e.target.value)}
+              placeholder="Type to search..."
+              value={searchTerm}
+              onChange={(e) => onSearchChange(e.target.value)}
             />
           </div>
-        );
+        )
       }
->>>>>>> main
 
 export default SearchBar
